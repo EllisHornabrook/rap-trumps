575fe3c770f507d4024408468cf3054bcda8e752
Ellis Hornabrook <ellishornabrook@gmail.com>
2021-01-25 19:25:49 +0000

    added styling to display who's turn it is

--- src/components/CardCount/CardCount.jsx
+++ src/components/CardCount/CardCount.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "./CardCount.module.scss";
 
 const CardCount = (props) => {
-    const { firstDeck, secondDeck } = props;
+    const { firstDeck, secondDeck, playerTurn } = props;
+    const hightlightTurn = playerTurn ? styles.player : styles.opponent;
 
     return (
-        <div className={styles.cardCount}>
-            <h2>{firstDeck.length}</h2>
-            <h2>{secondDeck.length}</h2>
+        <div className={`${styles.cardCount} ${hightlightTurn}`}>
+            <h2 className={styles.playerCount}>{firstDeck.length}</h2>
+            <h2 className={styles.opponentCount}>{secondDeck.length}</h2>
         </div>
     );
 };
